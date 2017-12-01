78ff440eaee2c6a0d040eb0976dc24db249d7dbd
Gavin420 <34166692+Gavin420@users.noreply.github.com>
2017-12-01 16:38:41 +0000

    Add files via upload

---
+++ sketch_dec01a/sketch_dec01a.ino
@@ -0,0 +1,20 @@
+#include <Adafruit_GPS.h> 
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(3, 2);
+Adafruit_GPS GPS(&mySerial);
+
+String NMEA1;
+String NMEA2;
+char c;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  GPS.begin(9600);
+  GPS.sendCommand("$PGCMD,33,0*6D");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
